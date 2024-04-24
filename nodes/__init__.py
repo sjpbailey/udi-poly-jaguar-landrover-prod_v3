4281bc6eb6ea4908707bfe2ba2f1ae4e441b0595
sjpbailey <sjpbailey1961@gmail.com>
2024-04-23 19:17:23 -0700

    Update __init__.py

--- nodes/__init__.py
+++ nodes/__init__.py
@@ -1,5 +1,6 @@
 
 """ Node classes used by the Python template Node Server. """
 
-from .JaguarNode            import JaguarNode
-from .JaguarController      import JaguarController
+from .jlrpy import jlrpy
+from .JaguarNode import JaguarNode
+from .JaguarController import JaguarController
